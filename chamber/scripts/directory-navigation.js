6d47fb2aed5b566a82f660dbbd5fb509cd93fba9
Eusebio Ngoy <banzaeusebio2@gmail.com>
2026-01-14 22:48:02 +0200

    add clients list

--- chamber/scripts/directory-navigation.js
+++ chamber/scripts/directory-navigation.js
@@ -41,3 +41,21 @@ async function fetchAndDisplayMembers() {
 
 // Call the function to start the process
 fetchAndDisplayMembers();
+const gridbutton = document.querySelector("#grid");
+const listbutton = document.querySelector("#list");
+const display = document.querySelector("article");
+
+// The following code could be written cleaner. How? We may have to simplfiy our HTMl and think about a default view.
+
+gridbutton.addEventListener("click", () => {
+    // example using arrow function
+    display.classList.add("grid");
+    display.classList.remove("list");
+});
+
+listbutton.addEventListener("click", showList); // example using defined function
+
+function showList() {
+    display.classList.add("list");
+    display.classList.remove("grid");
+}
