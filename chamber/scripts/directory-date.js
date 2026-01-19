6f3ad1b8f7789e79b3b23a8eab244f4ac55867c9
Eusebio Ngoy <banzaeusebio2@gmail.com>
2026-01-19 22:23:17 +0200

    add some sections

---
+++ chamber/scripts/directory-date.js
@@ -0,0 +1,34 @@
+const myTown = document.querySelector('#town');
+const myDescription = document.querySelector('#description');
+const myGraphic = document.querySelector('#graphic');
+const myTemperature = document.querySelector('#temperature');
+
+const myKey = "993e4bcdb158bfda5d922dc270f269ab"
+const myLat = "-11.68673"
+const myLong = "27.48088"
+
+const url = `//api.openweathermap.org/data/2.5/weather?lat=${myLat}&lon=${myLong}&appid=${myKey}&units=imperial`
+
+async function apiFetch() {
+    try {
+        const response = await fetch(url);
+        if (response.ok) {
+            const data = await response.json();
+            displayResults(data);
+        } else {
+            throw Error(await response.text());
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+function displayResults(data) {
+    myTown.innerHTML = data.name
+    myDescription.innerHTML = data.weather[0].description
+    myTemperature.innerHTML = `${data.main.temp}&deg;F`
+    const iconsrc = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
+    myGraphic.setAttribute('SRC', iconsrc)
+    myGraphic.setAttribute('alt', data.weather[0].description)
+}
+apiFetch();
