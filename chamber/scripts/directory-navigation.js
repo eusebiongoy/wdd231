cd0aebc47f0bed72dbd07f52ad6c3c9d2e080b96
Eusebio Ngoy <banzaeusebio2@gmail.com>
2026-01-26 23:42:34 +0200

    commit all

--- chamber/scripts/directory-navigation.js
+++ chamber/scripts/directory-navigation.js
@@ -1,10 +1,13 @@
 
+
+
+
 document.getElementById("lastModified").innerHTML = document.lastModified;
 
 const navbutton = document.querySelector('#ham-btn');
 const navlinks = document.querySelector('#nav-bar');
 
-navbutton.addEventListener('click', () => {
+navbutton.addEventListener('click',() => {
     navbutton.classList.toggle('show');
     navlinks.classList.toggle('show');
 });
