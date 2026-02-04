e89749eef8b434b07144ac3f65458c425847a782
Eusebio Ngoy <banzaeusebio2@gmail.com>
2026-02-04 08:06:59 +0200

    review some code

---
+++ chamber/data/discover.mjs
@@ -0,0 +1,62 @@
+
+        export const places =  [
+            {
+                "name": "Virunga National Park",
+                "address": "Virunga National Park Headquarters, Goma, North Kivu, DRC",
+                "description": "Africa’s oldest national park and a UNESCO World Heritage Site, famous for mountain gorillas, volcanoes, savannas, and rich biodiversity.",
+                "estimated_cost_usd": "Park activities vary; gorilla trekking approx. 400 USD",
+                "photo_url": "images/virunga.webp"
+                
+            },
+            {
+                "name": "Mount Nyiragongo",
+                "address": "Virunga National Park, near Goma, North Kivu, DRC",
+                "description": "One of the world’s most active volcanoes, known for its massive lava lake and guided hiking experiences.",
+                "estimated_cost_usd": "Volcano trek approx. 300–600 USD",
+                "photo_url": "images/nyiragongo.webp"
+            },
+            {
+                "name": "Kahuzi-Biega National Park",
+                "address": "Near Bukavu, South Kivu, DRC",
+                "description": "UNESCO World Heritage Site protecting eastern lowland gorillas and vast tropical rainforest ecosystems.",
+                "estimated_cost_usd": "Gorilla trekking permit approx. 400 USD",
+                "photo_url": "images/gorillatours.webp"
+            },
+            {
+                "name": "A Motel",
+                "address": "Lualaba River, near Kisangani, Tshopo Province, DRC",
+                "description": "A series of seven powerful cataracts on the Congo River, known for scenic beauty and traditional Wagenia fishing practices.",
+                "estimated_cost_usd": "Free (local guide fees may apply)",
+                "photo_url": "images/gorillapark.webp"
+            },
+            {
+                "name": "Lake Kivu & Idjwi Island",
+                "address": "Lake Kivu, near Goma and Bukavu, Eastern DRC",
+                "description": "Scenic freshwater lake with beaches, boat rides, and the peaceful Idjwi Island, ideal for cycling and cultural tours.",
+                "estimated_cost_usd": "Free access; boat tours vary",
+                "photo_url": "images/congoSafari.webp"
+
+            },
+            {
+                "name": "Inga Central Energy",
+                "address": "Les Petites Chutes de Lukaya, Kinshasa, DRC",
+                "description": "The world’s only sanctuary dedicated to rescuing and rehabilitating orphaned bonobos.",
+                "estimated_cost_usd": "Entrance approx. 10 USD",
+                "photo_url": "images/inga.webp"
+            },
+            {
+                "name": "Salonga National Park",
+                "address": "Central Congo Basin, DRC",
+                "description": "The largest tropical rainforest reserve in Africa, home to bonobos, forest elephants, and rare wildlife.",
+                "estimated_cost_usd": "Costs vary due to remote access and guided expeditions",
+                "photo_url": "images/okapi.webp"
+            },
+            {
+                "name": "Unity Places",
+                "address": "Lubumbashi, DRC",
+                "description": "Known as the copper capital and here is the place showing the unity of four provinces born from this area.",
+                "estimated_cost_usd": "Variable (depends on activities)",
+                "photo_url": "images/lubumbashi.webp"
+            }
+        ]
+
